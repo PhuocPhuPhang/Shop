39768cb52f1014bfb33a04b72000a8c94352d82e
= <=>
2019-07-07 22:06:24 +0700

    Cập nhật

--- resources/views/admin/sanpham/them.blade.php
+++ resources/views/admin/sanpham/them.blade.php
@@ -27,13 +27,6 @@
                 </div>
                 <div class="x_content form-horizontal form-label-left">
 
-                    <!-- <div class="form-group">
-                        <label class="control-label col-md-3 col-sm-3 col-xs-12">Mã sản phẩm</label>
-                        <div class="col-md-6 col-sm-6 col-xs-12">
-                            <input type="text" name="ma" id="ma" class="form-control col-md-7 col-xs-12 inputForm">
-                        </div>
-                    </div> -->
-
                     <div class="form-group">
                         <label class="control-label col-md-3 col-sm-3 col-xs-12">Tên sản phẩm</label>
                         <div class="col-md-6 col-sm-6 col-xs-12">
@@ -61,12 +54,12 @@
                     <div class="form-group">
                         <label class="control-label col-md-3 col-sm-3 col-xs-12">Số lượng</label>
                         <div class="col-md-2 col-sm-6 col-xs-12">
-                            <input type="text" name="soluong" id="soluong" class="form-control col-md-7 col-xs-12 inputForm">
+                            <input type="text" name="soluong" id="soluong" onkeyup="this.value=this.value.replace(/[^\d]/,'')" class="form-control col-md-7 col-xs-12 inputForm">
                         </div>
 
                         <label class="control-label col-md-2 col-sm-3 col-xs-12">Giá bán</label>
                         <div class="col-md-2 col-sm-6 col-xs-12">
-                            <input type="text" name="giaban" id="giaban" class="form-control col-md-7 col-xs-12 inputForm">
+                            <input type="text" name="giaban" id="giaban" onkeyup="this.value=this.value.replace(/[^\d]/,'')" pattern="^\$\d{1,3}(,\d{3})*(\.\d+)?$" value="" data-type="currency" class="form-control col-md-7 col-xs-12 inputForm">
                         </div>
                     </div>
 
@@ -344,5 +337,55 @@
             return str;
         }
     });
+
+    $("input[data-type='currency']").on({
+        keyup: function() {
+            formatCurrency($(this));
+        },
+        blur: function() {
+            formatCurrency($(this), "blur");
+        }
+    });
+
+
+    function formatNumber(n) {
+        // format number 1000000 to 1,234,567
+        return n.replace(/\D/g, "").replace(/\B(?=(\d{3})+(?!\d))/g, ",")
+    }
+
+    function formatCurrency(input, blur) {
+        var input_val = input.val();
+        if (input_val === "") {
+            return;
+        }
+        var original_len = input_val.length;
+        var caret_pos = input.prop("selectionStart");
+
+        if (input_val.indexOf(".") >= 0) {
+            var decimal_pos = input_val.indexOf(".");
+            var left_side = input_val.substring(0, decimal_pos);
+            var right_side = input_val.substring(decimal_pos);
+            left_side = formatNumber(left_side);
+            right_side = formatNumber(right_side);
+
+            if (blur === "blur") {
+                right_side += "00";
+            }
+
+            right_side = right_side.substring(0, 2);
+            input_val = left_side + "." + right_side;
+
+        } else {
+            input_val = formatNumber(input_val);
+            input_val = input_val;
+            if (blur === "blur") {
+                input_val += ".00";
+            }
+        }
+        input.val(input_val);
+        var updated_len = input_val.length;
+        caret_pos = updated_len - original_len + caret_pos;
+        input[0].setSelectionRange(caret_pos, caret_pos);
+    }
 </script>
 @endsection
